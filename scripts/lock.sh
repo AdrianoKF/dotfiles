38a8d944e3e9356474fceb05e39e2ad71436292e
Adrian Rumpold <a.rumpold@gmail.com>
2019-08-26 12:38:57 +0200

    Fix date display on lock screen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -7,40 +7,32 @@ T="$(xrdb -query | grep '*.foreground' | cut -f 2)ee"  # text
 W='#880000bb'  # wrong
 V="$(xrdb -query | grep '*.color1:' | cut -f 2)bb"  # verifying
 
-i3lock_color() {
-	i3lock \
-		--insidevercolor=$C   \
-		--ringvercolor=$V     \
-		\
-		--insidewrongcolor=$C \
-		--ringwrongcolor=$W   \
-		\
-		--insidecolor=$D      \
-		--ringcolor=$D        \
-		--linecolor=$B        \
-		--separatorcolor=$D   \
-		\
-		--verifcolor=$T        \
-		--wrongcolor=$T        \
-		--timecolor=$T        \
-		--datecolor=$T        \
-		--layoutcolor=$T      \
-		--keyhlcolor=$V       \
-		--bshlcolor=$V        \
-		\
-		--blur 20              \
-		--clock               \
-		--indicator           \
-		--timestr="%H:%M:%S"  \
-		--datestr="%A, %m %Y" \
-		--keylayout 2         \
-		# --veriftext="Drinking verification can..."
-		# --wrongtext="Nope!"
-		# --textsize=20
-		# --modsize=10
-		# --timefont=comic-sans
-		# --datefont=monofur
-}
+i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$D      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$V       \
+--bshlcolor=$V        \
+\
+--blur 20              \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %Y-%m-%d" \
+--keylayout 2         \
 
 i3lock_fancy_rapid() {
 	~/scripts/i3lock-fancy-rapid 16 2
