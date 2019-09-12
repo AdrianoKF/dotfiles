1a46f484afd97189f8fa4771dcd5c6154d5b17b7
Adrian Rumpold <a.rumpold@gmail.com>
2019-09-12 11:09:12 +0200

    Multi-monitor handling and profile switching

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -3,10 +3,18 @@
 # Bail out on error
 set -e
 
+eDP_STATUS=$(</sys/class/drm/card0/card0-eDP-1/status)
 DP_LEFT_STATUS=$(</sys/class/drm/card0/card0-DP-2/status)
 DP_RIGHT_STATUS=$(</sys/class/drm/card0/card0-DP-3/status)
 
+echo "-------------- $(date) -------------"
+echo "eDP Status: $eDP_STATUS" >> /tmp/monitorlog
 echo "LEFT Status: $DP_LEFT_STATUS" >> /tmp/monitorlog
 echo "RIGHT Status: $DP_RIGHT_STATUS" >> /tmp/monitorlog
 
-DISPLAY=:0 /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "Something happened"
+# Invoke autorandr if present
+if [ -x /usr/bin/autorandr ]; then
+    /usr/bin/autorandr --change --batch
+    DISPLAY=:0 /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "Running autorandr"
+fi
+
