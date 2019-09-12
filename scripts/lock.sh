9cf8a8ef268a2e743a676c0ceacd1e1a89520747
Adrian Rumpold <a.rumpold@gmail.com>
2019-09-12 11:24:18 +0200

    Autolocking

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -7,33 +7,6 @@ T="$(xrdb -query | grep '*.foreground' | cut -f 2)ee"  # text
 W='#880000bb'  # wrong
 V="$(xrdb -query | grep '*.color1:' | cut -f 2)bb"  # verifying
 
-i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
-\
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
-\
---insidecolor=$D      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
-\
---verifcolor=$T        \
---wrongcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$V       \
---bshlcolor=$V        \
-\
---blur 20              \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %Y-%m-%d" \
---keylayout 2         \
-
 i3lock_fancy_rapid() {
 	~/scripts/i3lock-fancy-rapid 16 2
 }
