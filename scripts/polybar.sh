910c848336c41d2342b10ab7f4d232a8dfe432ba
Adrian Rumpold <a.rumpold@gmail.com>
2019-09-19 10:01:27 +0200

    Polybar: Better tray positioning, colors from Xresources

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,19 +1,47 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# Terminate already running bar instances
-killall -q polybar
+(
+	# Prevent race conditions
+	flock 200
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+	# Terminate already running bar instances
+	killall -q polybar
 
-# Launch polybar
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
-BAR_NAME=top
+	# Preferred output
+	preferred_tray_output=DP1-1
+	fallback_tray_output=eDP1
 
-if [[ -x /usr/bin/xrandr ]]; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload $BAR_NAME &
-  done
-else
-  polybar --reload $BAR_NAME &
-fi
+	BAR_NAME=top
+
+	if [[ -x /usr/bin/xrandr ]]; then
+		outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+		tray_output=$fallback_tray_output
+
+		# Set preferred tray output, if connected
+		for m in $outputs; do
+			if [[ $m == $preferred_tray_output ]]; then
+				tray_output=$m
+			fi
+		done
+
+		# Reload polybar with tray on preferred output
+		for m in $outputs; do
+			export MONITOR=$m
+			export TRAY_POSITION=none
+			if [[ $m == $tray_output ]]; then
+				TRAY_POSITION=right
+			fi
+
+			# Reload polybar and release the lock
+			polybar --reload $BAR_NAME </dev/null >/tmp/polybar-$m.log 2>&1 200>&- &
+			disown
+		done
+	else
+		# Just reload polybar and release the lock
+		polybar --reload $BAR_NAME </dev/null >/tmp/polybar.log 2>&1 200>&- &
+		disown
+	fi
+) 200>/tmp/polybar-launcher.lock
