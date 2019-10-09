cf0148ddebde1160eae92736e6b798936b4d20b2
Adrian Rumpold <a.rumpold@gmail.com>
2019-10-09 10:11:30 +0200

    Accidentally deleted lock script

---
+++ scripts/lock.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D="$(xrdb -query | grep '*.background' | cut -f 2)cc"  # default
+T="$(xrdb -query | grep '*.foreground' | cut -f 2)ee"  # text
+W='#880000bb'  # wrong
+V="$(xrdb -query | grep '*.color1:' | cut -f 2)bb"  # verifying
+
+lock_screen() {
+	~/scripts/i3lock-fancy-rapid 16 2 -n
+}
+
+pause_dunst() {
+	notify-send "DUNST_COMMAND_PAUSE"
+}
+
+unpause_dunst() {
+	notify-send "DUNST_COMMAND_RESUME"
+}
+
+pause_dunst
+lock_screen
+unpause_dunst
