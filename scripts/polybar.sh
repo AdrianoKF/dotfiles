7c0663f9f32482d84f02f12d28a2971a78838c63
Adrian Rumpold <adrian.rumpold@unternehmertum.de>
2019-11-27 18:08:21 +0100

    Polybar: Preferred positioning for aAI setup

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -11,8 +11,8 @@
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 	# Preferred output
-	preferred_tray_output=DP1-1
-	fallback_tray_output=eDP1
+	preferred_tray_output=DP-2
+	fallback_tray_output=DP-0
 
 	BAR_NAME=top
 
