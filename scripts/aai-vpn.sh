5e397027af281ed514917f581cbb70c4fb65bbb9
Adrian Rumpold <a.rumpold@gmail.com>
2020-04-16 16:28:22 +0200

    New revision of aAI VPN script, requires vpn-splice

--- scripts/aai-vpn.sh
+++ scripts/aai-vpn.sh
@@ -3,8 +3,10 @@
 sudo openconnect --user rumpold \
 	--passwd-on-stdin \
 	--protocol gp \
-	--verbose \
-	--script=/usr/local/bin/vpnc-script \
+        --background \
+	--script="/usr/bin/vpn-slice 10.0.0.0/8 172.0.0.0/8" \
 	--servercert pin-sha256:lB/2vmLRPBhyGPv56CeveBmbAyRU/7+OSTtGISg2JtA= \
-	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN')
+	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN') && \
+sleep 1 && \
+sudo systemd-resolve --interface tun0 --set-domain aai.lab --set-dns 10.32.16.1 --set-dnssec no
 
