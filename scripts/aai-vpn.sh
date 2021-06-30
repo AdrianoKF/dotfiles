fe3c8b022db92aba54266354de2aa05e6de6f329
Adrian Rumpold <a.rumpold@gmail.com>
2021-06-30 08:05:20 +0200

    Update VPN script for newest gateway

--- scripts/aai-vpn.sh
+++ scripts/aai-vpn.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+set -xeu
+
+# sudo openconnect --user rumpold \
+# 	--passwd-on-stdin \
+# 	--protocol gp \
+#         --background \
+# 	--script="/usr/bin/vpn-slice 10.0.0.0/8 172.20.0.0/16" \
+# 	--servercert pin-sha256:lB/2vmLRPBhyGPv56CeveBmbAyRU/7+OSTtGISg2JtA= \
+# 	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN') && \
+# sudo openconnect --user rumpold \
+# 	--passwd-on-stdin \
+#         --protocol gp \
+#         --background \
+#         --authgroup=vpn01-le \
+# 	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
+# 	--servercert pin-sha256:vyJLuE3cdK+VXrlhe+B9pBny+3gnFn8TPEGWuEuSpyU= \
+# 	vpn01.aai.sh < <(secret-tool lookup service 'aAI VPN') && \
 sudo openconnect --user rumpold \
 	--passwd-on-stdin \
 	--protocol gp \
-        --background \
-	--script="/usr/bin/vpn-slice 10.0.0.0/8 172.0.0.0/8" \
-	--servercert pin-sha256:lB/2vmLRPBhyGPv56CeveBmbAyRU/7+OSTtGISg2JtA= \
-	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN') && \
-sleep 1 && \
-sudo systemd-resolve --interface tun0 --set-domain aai.lab --set-dns 10.32.16.1 --set-dnssec no
+	--background \
+	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
+	--servercert pin-sha256:CJUmSp3ZEjp23xgtMIToaZJDGr4xO8b89YYRVSbfjHw= \
+	vpn02.aai.sh < <(secret-tool lookup service 'aAI VPN')
 
+K8S_MASTER=$(dig k8s.infra.aai.sh +short | head -n1)
+sudo ip route add $K8S_MASTER dev tun0
