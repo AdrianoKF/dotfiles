d5c585bce880870222cc8b2fdfdb03aa46aceff4
Adrian Rumpold <a.rumpold@gmail.com>
2021-09-10 08:22:05 +0200

    vpn: Fixes

--- scripts/aai-vpn.sh
+++ scripts/aai-vpn.sh
@@ -9,21 +9,22 @@ set -xeu
 # 	--script="/usr/bin/vpn-slice 10.0.0.0/8 172.20.0.0/16" \
 # 	--servercert pin-sha256:lB/2vmLRPBhyGPv56CeveBmbAyRU/7+OSTtGISg2JtA= \
 # 	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN') && \
-# sudo openconnect --user rumpold \
-# 	--passwd-on-stdin \
-#         --protocol gp \
-#         --background \
-#         --authgroup=vpn01-le \
-# 	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
-# 	--servercert pin-sha256:vyJLuE3cdK+VXrlhe+B9pBny+3gnFn8TPEGWuEuSpyU= \
-# 	vpn01.aai.sh < <(secret-tool lookup service 'aAI VPN') && \
 sudo openconnect --user rumpold \
 	--passwd-on-stdin \
-	--protocol gp \
-	--background \
+        --protocol gp \
+        --background \
+        --authgroup=vpn01-le \
 	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
-	--servercert pin-sha256:CJUmSp3ZEjp23xgtMIToaZJDGr4xO8b89YYRVSbfjHw= \
-	vpn02.aai.sh < <(secret-tool lookup service 'aAI VPN')
+	--servercert pin-sha256:+9ZWopAZddZv+scPlYA+qpcAdNJcm8GdMtV5NlRbEjc= \
+	vpn01.aai.sh < <(secret-tool lookup service 'aAI VPN') && \
+#sudo openconnect --user rumpold \
+#	--passwd-on-stdin \
+#	--protocol gp \
+#	--background \
+#	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
+#	--servercert pin-sha256:CJUmSp3ZEjp23xgtMIToaZJDGr4xO8b89YYRVSbfjHw= \
+#	vpn02.aai.sh < <(secret-tool lookup service 'aAI VPN')
 
+sleep 1
 K8S_MASTER=$(dig k8s.infra.aai.sh +short | head -n1)
 sudo ip route add $K8S_MASTER dev tun0
