30ae7f76344f4b0b3789e10ccd69ee2a0220b9ac
Adrian Rumpold <a.rumpold@gmail.com>
2022-06-22 07:54:27 +0200

    fix(VPN): Remove dead code, no cert fingerprint validation

--- scripts/aai-vpn.sh
+++ scripts/aai-vpn.sh
@@ -5,33 +5,12 @@ set -xeu
 GATEWAY=vpn01.aai.sh
 AUTHGROUP=vpn01-le
 
-# GATEWAY=vpn02.aai.sh
-# CERT_FINGERPRINT="pin-sha256:QNKZ7jW3JXZCqtvjFDhJoBwm60ZQ3OA28dBuPRpHzAU="
-CERT_FINGERPRINT="pin-sha256:qKXYOg+Tb/8tbWvBS/lQuY/6RUB5OUUGYgmdiBoMrKk="
-#CERT_FINGERPRINT="pin-sha256:+9ZWopAZddZv+scPlYA+qpcAdNJcm8GdMtV5NlRbEjc="
-
-# sudo openconnect --user rumpold \
-# 	--passwd-on-stdin \
-# 	--protocol gp \
-#         --background \
-# 	--script="/usr/bin/vpn-slice 10.0.0.0/8 172.20.0.0/16" \
-# 	--servercert pin-sha256:lB/2vmLRPBhyGPv56CeveBmbAyRU/7+OSTtGISg2JtA= \
-# 	vpn01.appliedai.de < <(secret-tool lookup service 'aAI VPN') && \
-#sudo openconnect --user rumpold \
-#	--passwd-on-stdin \
-#        --protocol gp \
-#        --background \
-#        --authgroup=vpn01-le \
-#	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
-#	--servercert pin-sha256:+9ZWopAZddZv+scPlYA+qpcAdNJcm8GdMtV5NlRbEjc= \
-#	vpn01.aai.sh < <(secret-tool lookup service 'aAI VPN') && \
 sudo openconnect --user rumpold \
 	--passwd-on-stdin \
 	--protocol gp \
 	--background \
 	--script="/usr/bin/vpn-slice 10.0.0.0/8" \
 	--authgroup=$AUTHGROUP \
-	--servercert $CERT_FINGERPRINT \
 	$GATEWAY < <(secret-tool lookup service 'aAI VPN')
 
 sleep 1
